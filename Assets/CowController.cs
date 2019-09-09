1c5cdfb28dd3cc8fc843b2f0ba1dfc630785f903
Marco <marco.venegas98@hotmail.com>
2019-09-09 00:09:36 -0600

    Some art assets, running kid and random rotation and track spawning cows

---
+++ Assets/CowController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CowController : MonoBehaviour
+{
+    public int track; //Use to place on map according to its track.
+    // Start is called before the first frame update
+    void Start()
+    {
+    	//Determine orientation
+        if(Random.Range(0.0f, 1.0f) < 0.5f){
+        	transform.Rotate(0, 90.0f, 0);
+        }else{
+        	transform.Rotate(0, -90.0f, 0);
+        }
+
+        //Determine track
+        float laneRandom = Random.Range(0.0f, 1.0f);
+        if(laneRandom < 0.33){
+        	track = 0;
+        }else if(laneRandom < 0.66){
+        	track = 1;
+    	}else{
+    		track = 2;
+    	}
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/KidController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KidController : MonoBehaviour
+{	
+	public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GetComponent<Rigidbody>().AddRelativeForce(0, 0, speed);
+    }
+}
