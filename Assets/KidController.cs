da4af09a3e986f28777bcfbd7dc7bddf96d8241d
Kevin <kevinja9608@gmail.com>
2019-09-15 01:29:37 -0600

    It can move to the right center and left

--- Assets/KidController.cs
+++ Assets/KidController.cs
@@ -4,16 +4,65 @@ using UnityEngine;
 
 public class KidController : MonoBehaviour
 {	
-	public float speed;
-    // Start is called before the first frame update
-    void Start()
+	public float speed = 10;
+	public Vector3 left = new Vector3(-9, (float)3.4393, 0);
+	public Vector3 center = new Vector3(0, (float)3.4393, 0);
+	public Vector3 right = new Vector3(9, (float)3.4393, 0);
+	public Vector3 currentPosition;
+	public Vector3 desiredPosition;
+	public float distance = 0;
+	public bool isJumping = false; 
+	private Rigidbody rb;
+	// Start is called before the first frame update
+	void Start()
     {
+		rb = GetComponent<Rigidbody>();
+		rb.position = center;
+		currentPosition = center;
+		desiredPosition = center;
+	}
 
-    }
+	void Update()
+	{
+		distance = Vector3.Distance(rb.position, desiredPosition);
+		
+		if (distance < 0.15)
+		{
+			currentPosition = desiredPosition;
+			if (Input.GetAxis("Horizontal") < 0)
+			{
+				if (currentPosition.Equals(center))
+				{
+					desiredPosition = left;
+				}
+				else if (currentPosition.Equals(right))
+				{
+					desiredPosition = center;
+				}
 
-    // Update is called once per frame
-    void Update()
+			}
+			else if (Input.GetAxis("Horizontal") > 0)
+			{
+				if (currentPosition.Equals(center))
+				{
+					desiredPosition = right;
+				}
+				else if (currentPosition.Equals(left))
+				{
+					desiredPosition = center;
+				}
+
+			}
+		}
+		else
+		{
+			rb.position = Vector3.MoveTowards(transform.position, desiredPosition, maxDistanceDelta: Time.fixedDeltaTime * speed);
+		}
+		
+	}
+// Update is called once per frame
+void FixedUpdate()
     {
-        //GetComponent<Rigidbody>().AddRelativeForce(0, 0, speed);
-    }
+		
+	}
 }
