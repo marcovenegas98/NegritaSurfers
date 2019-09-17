736cb81f3cbcb59920da4fd0284f8ff6e4e4106f
Kevin <kevinja9608@gmail.com>
2019-09-17 15:22:13 -0600

    Jump, animations still needs work

--- Assets/KidController.cs
+++ Assets/KidController.cs
@@ -7,21 +7,30 @@ public class KidController : MonoBehaviour
 {
 	enum Position { LEFT = -1, CENTER = 0, RIGHT = 1};
 	[SerializeField]
-	public float fowardSpeed = 10;
-	public float horizontalSpeed = 30;
-	public float distanceLanes = 9;
+	public float fowardSpeed = 10f;
+	public float horizontalSpeed = 30f;
+	public float distanceLanes = 9f;
+	public float fallMultiplier = 2.5f;
+	public float jumpForce = 20f;
 	private Position current;
-	private Rigidbody rb;
 	private Vector3 desiredDirection;
+	public Animation anim;
+	private Rigidbody rb;
+	public GameObject ob;
+
 	// Start is called before the first frame update 
 	void Start()
 	{
 		rb = GetComponent<Rigidbody>();
 		current = Position.CENTER;
+		anim = ob.GetComponent<Animation>();
 	}
 
 	void Update()
 	{
+		if (rb.velocity.y < 0) {
+			rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+		}
 		if (Input.GetKeyDown(KeyCode.A)  && current != Position.LEFT)
 		{
 			current--;
@@ -31,10 +40,20 @@ public class KidController : MonoBehaviour
 			current++;
 		}
 
+		if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+		{
+			rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+			anim.CrossFade("Jump");
+		}
+		
 		desiredDirection = new Vector3(distanceLanes * (int)current, rb.position.y, rb.position.z);
 
 		rb.position = Vector3.MoveTowards(rb.position, desiredDirection, maxDistanceDelta: Time.deltaTime * horizontalSpeed);
 		//rb.position = Vector3.MoveTowards(rb.position, rb.position + Vector3.forward, maxDistanceDelta: Time.deltaTime * fowardSpeed);
+	}
 
+	private bool IsGrounded()
+	{
+		return rb.velocity.y == 0;
 	}
 }
