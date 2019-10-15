4bdcd83239e9626d9c8a8749583d745d0589e4d9
Marco <marco.venegas98@hotmail.com>
2019-10-15 11:27:56 -0600

    Fixed terrain, and goats with movement in joes branch :v

--- Assets/src/Goat.cs
+++ Assets/src/Goat.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Goat : Obstacle
+public class Goat : ObstacleX
 {
 	public float timeSwitchingTracks;
 
@@ -20,7 +20,7 @@ public class Goat : Obstacle
     {
         base.Start();
     	moving = false;
-        startPosition = target = transform.position;
+        startPosition = target = transform.localPosition;
         coroutine = changeTrack();
         StartCoroutine(coroutine);
     }
@@ -28,9 +28,9 @@ public class Goat : Obstacle
     // Update is called once per frame
     void Update()
     {
-    	if(moving && (transform.position != target)){ //Move to the target
+    	if(moving && (transform.localPosition != target)){ //Move to the target
     		t += Time.deltaTime/timeSwitchingTracks; 
-        	transform.position = Vector3.Lerp(startPosition, target, t);
+        	transform.localPosition = Vector3.Lerp(startPosition, target, t);
     	}else{
     		moving = false;
     	}
@@ -43,25 +43,25 @@ public class Goat : Obstacle
     			Vector3 destination = new Vector3(0.0f, 0.0f, 0.0f);
 		    	switch(track){
 		    		case TracksEnum.LEFT: {//Left track, move to middle track, looking left
-		    			destination = new Vector3(transform.position.x + btwnTrackDistance, transform.position.y, transform.position.z);
+		    			destination = new Vector3(transform.localPosition.x + btwnTrackDistance, transform.localPosition.y, transform.localPosition.z);
 		    			changeOrientationRight();
 		    			track = TracksEnum.MIDDLE;
 		    		}break;
 		    		case TracksEnum.MIDDLE: { //Middle track, move to either
 		    			if(Random.Range(0.0f, 1.0f) < 0.5f){
 		    				//Move to left
-		    				destination = new Vector3(transform.position.x - btwnTrackDistance, transform.position.y, transform.position.z);
+		    				destination = new Vector3(transform.localPosition.x - btwnTrackDistance, transform.localPosition.y, transform.localPosition.z);
 		    				changeOrientationLeft();
 		    				track = TracksEnum.LEFT;
 						}else{
 							//Move to right
-							destination = new Vector3(transform.position.x + btwnTrackDistance, transform.position.y, transform.position.z);
+							destination = new Vector3(transform.localPosition.x + btwnTrackDistance, transform.localPosition.y, transform.localPosition.z);
 							changeOrientationRight();
 							track = TracksEnum.RIGHT;
 						}
 		    		}break;
 		    		case TracksEnum.RIGHT: {//Right track, move to middle track
-		    			destination = new Vector3(transform.position.x - btwnTrackDistance, transform.position.y, transform.position.z);
+		    			destination = new Vector3(transform.localPosition.x - btwnTrackDistance, transform.localPosition.y, transform.localPosition.z);
 		    			changeOrientationLeft();
 		    			track = TracksEnum.MIDDLE;
 		    		}break;
@@ -86,7 +86,7 @@ public class Goat : Obstacle
 
     private void setDestination(Vector3 destination, float time){
             t = 0;
-            startPosition = transform.position;
+            startPosition = transform.localPosition;
             timeSwitchingTracks = time;
             target = destination; 
     }
