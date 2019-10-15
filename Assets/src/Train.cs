4db277a609dd32ac1854f678353eaeac4906a7a3
Marco <marco.venegas98@hotmail.com>
2019-10-15 07:06:19 -0600

    Train

---
+++ Assets/src/Train.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Train : Obstacle
+{
+	public bool moves; //Every train should be told if it moves or not
+	private static Vector3 kidPosition = new Vector3(0f,0f,0f);
+	private static float nearFactor = 50f;
+	void Start()
+	{
+		base.Start();
+	}
+
+	void Update()
+	{
+		if(isNearPlayer()){
+			move();
+		}
+	}
+
+	private bool isNearPlayer(){
+		bool result = false;
+		if(transform.position.z < nearFactor){
+			result = true;
+		}
+		return result;
+	}
+
+	private void move(){
+		Debug.Log("Se mueve");
+	}
+
+}
