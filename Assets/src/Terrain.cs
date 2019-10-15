0939d52a4eaeeabdea169e5f5049539f54e0a01b
Marco <marco.venegas98@hotmail.com>
2019-10-15 14:59:40 -0600

    Train works :D

--- Assets/src/Terrain.cs
+++ Assets/src/Terrain.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Terrain : MonoBehaviour
 {
-    private float speed = 0.125f;
+    private float speed = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
