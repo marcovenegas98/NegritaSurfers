4fb59424458e26446453b2f38d580340d9b6e37d
Joseph Rementería <b55824@ecci.ucr.ac.cr>
2019-10-29 12:39:52 -0600

    the same as before but with a list

--- Assets/src/RandomGenerator.cs
+++ Assets/src/RandomGenerator.cs
@@ -6,15 +6,23 @@ public class RandomGenerator : MonoBehaviour
 {
     public GameObject l1_1;
     public GameObject l2_1;
-    private List<GameObject> prefabList = new List<GameObject>();
+    private List<GameObject> levelsList = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(l1_1, new Vector3(80, 0, 253), new Quaternion(0,0,0,1));
-        Instantiate(l2_1, new Vector3(80, 0, (253+250)), new Quaternion(0, 0, 0, 1));
-        //Instantiate(this.l1_1);
-        //Instantiate(this.l2_1);
+        // filling the list
+        this.levelsList.Add(l1_1);
+        this.levelsList.Add(l2_1);
+
+        Quaternion quat = new Quaternion(0,0,0,1);
+        for (int index = 0; index<2; ++index)
+        {
+            Instantiate(levelsList[(index)], new Vector3(80, 0, (253 + 250 * index)), quat);
+        }
+
+        //Instantiate(l1_1, new Vector3(80, 0, 253), Quaternion);
+        //Instantiate(l2_1, new Vector3(80, 0, (253+250)), new Quaternion(0, 0, 0, 1));
     }
 
     // Update is called once per frame
