ec1fd695c146e8577ee97971f6ceb44532be1df0
Daniel Díaz <danidiaza@gmail.com>
2019-10-29 21:03:08 -0600

    Animaciones y assets de la papa

---
+++ Assets/src/Potato/FloatAnimation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatAnimation : StateMachineBehaviour
+{
+    // public float Ydist = 5;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    // override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    // {
+    //     var transform = animator.GetComponent<Transform>();
+    //     Vector3 newPos = new Vector3();
+    //     newPos[0] = transform.localPosition[0];
+    //     newPos[1] = Ydist + animator.Yoff;
+    //     newPos[2] = transform.localPosition[2];
+    //     transform.localPosition = newPos;
+       
+    // }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
---
+++ Assets/src/Potato/PotatoController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotatoController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animator animator;
+    public float localYPos = 5;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var newPos = new Vector3(transform.localPosition.x, localYPos + animator.GetFloat("Yoff"), transform.localPosition.z);
+        transform.localPosition = newPos;
+    }
+}
