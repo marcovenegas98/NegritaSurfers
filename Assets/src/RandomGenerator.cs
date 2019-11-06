44f7e769691c0acbb34ccd6806e2bde3bcf39f6b
Daniel Díaz <danidiaza@gmail.com>
2019-11-05 22:05:54 -0600

    Prefabs de mis niveles

--- Assets/src/RandomGenerator.cs
+++ Assets/src/RandomGenerator.cs
@@ -8,7 +8,7 @@ public class RandomGenerator : MonoBehaviour
     private static int TOTAL_CHUNKS_PER_LEVEL = 8; //2 chunks per dev, per level
     private static int LEVELS = 3;
     private static int TOTAL_CHUNKS = LEVELS * TOTAL_CHUNKS_PER_LEVEL;
-    private static int TOTAL_PREFABS_PER_LEVEL = 1;
+    private static int TOTAL_PREFABS_PER_LEVEL = 8;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +17,7 @@ public class RandomGenerator : MonoBehaviour
         int listLeftLimit, listRightLimit, nextInt;
         Quaternion quat = new Quaternion(0,0,0,1);
 
-        for (int index = 0; index< TOTAL_CHUNKS; ++index)
+        for (int index = 0; index < TOTAL_CHUNKS; ++index)
         {
             listLeftLimit = Mathf.RoundToInt(Mathf.Floor(index / TOTAL_CHUNKS_PER_LEVEL) * TOTAL_PREFABS_PER_LEVEL);
             listRightLimit = listLeftLimit + TOTAL_PREFABS_PER_LEVEL;
