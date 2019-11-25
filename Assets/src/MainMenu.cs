55ed569c55c60bc023f5e216a6be3be89810a147
Kevin <kevinja9608@gmai.com>
2019-11-25 16:46:24 -0600

    meni

---
+++ Assets/src/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public void NewGame()
+	{
+		SceneManager.LoadScene(1);
+	}	
+	
+	public void QuitGame()
+	{
+		Debug.Log("Quit game!!!");
+		Application.Quit();
+	}
+}
---
+++ Assets/src/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+	public GameObject pauseMenu;
+	private bool isPaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+		ShowPauseMenu();
+		isPaused = false;
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			isPaused = !isPaused;
+			ShowPauseMenu();
+		}
+    }
+
+	public void ShowPauseMenu()
+	{
+		pauseMenu.SetActive(isPaused);
+	}
+
+	public void NewGame()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log("Quit game!!!");
+		SceneManager.LoadScene(0);
+	}
+}
